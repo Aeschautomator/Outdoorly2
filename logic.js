eb8bf811441e09afc24e6eeed4e1d8606c3f5793
Lex Chuthor <psc9qb@gmail.com>
2018-09-22 10:42:40 -0400

    combined logic files and added a map index

--- logic.js
+++ logic.js
@@ -1,3 +1,33 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBjD6alEMUlP0u9ubyVRe1w_V-aT8WF9Zo",
+  authDomain: "outdoorly2-1537372469520.firebaseapp.com",
+  databaseURL: "https://outdoorly2-1537372469520.firebaseio.com",
+  projectId: "outdoorly2-1537372469520",
+  storageBucket: "outdoorly2-1537372469520.appspot.com",
+  messagingSenderId: "982019906690"
+};
+firebase.initializeApp(config);
+
+var map;
+
+var mapUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=3461+Corley+Home+Drive,+Richmond,+VA&key=AIzaSyB8KmcbXCn9lulhHKjc593b5nskOQLDAIw";
+
+function initMap() {
+    $.ajax({
+    url: mapUrl,
+    method: "GET"
+}).then(function (response) {
+    var lat = response.results[0].geometry.location.lat;
+    var lng = response.results[0].geometry.location.lng;
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: lat, lng: lng },
+        zoom: 8
+    });
+});
+}
+
+
 var url = "https://api.nasa.gov/planetary/apod?api_key=6NprwCeXJI4VjBtN7Sgn2stKHJZGCiUF6JIToxzr";
 
 
@@ -30,8 +60,4 @@ $.ajax({
 $(document).on("click", "#apod_img_id", function(){
     $("#apod_img_id").wrap("<a href='apod.html'></a>");
 });
-
-// $("#apod_image_id").click(function() {
-//     alert("clicked");
-// });
 
